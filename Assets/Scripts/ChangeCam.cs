8e19c7ce6878379875859a630ed14213c4aeb4df
gmlluk <llucgrifol@gmail.com>
2024-10-20 18:28:37 +0200

    codigo avanzado de camera

--- Assets/Scripts/ChangeCam.cs
+++ Assets/Scripts/ChangeCam.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ChangeCam : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera cameraprimerapersona;  
+    public Camera cameratercerapersona;  
+
+    //començar amb primera persona
+    private bool isFirstPerson = true;
+
     void Start()
     {
-        //hola
+        //camera primera persona activa principi 
+        cameraprimerapersona.gameObject.SetActive(true);
+        cameratercerapersona.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+         if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SwitchCamera();
+        }
     }
+
+void SwitchCamera()
+    {
+
+        // Cambia estat de les camares
+        //Activar desactivar camaras
+       
+    }
+
 }
