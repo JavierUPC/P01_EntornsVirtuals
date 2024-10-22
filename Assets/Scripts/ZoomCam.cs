b593b2aa59afb5705600ccddaff148269710d404
gmlluc <lluc.grifol@estudiantat.upc.edu>
2024-10-22 12:07:18 +0200

    creacion del codigo de zoom

---
+++ Assets/Scripts/ZoomCam.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCam : MonoBehaviour
+{
+    
+    public 
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ZoomCam();
+    }
+
+    private void ZoomCam()
+    {
+
+    }
+
+}
