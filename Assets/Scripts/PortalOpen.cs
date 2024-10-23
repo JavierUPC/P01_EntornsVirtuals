5a763562130e46e28141adadde069f08ba79a418
Angl0d <javier.ingelius@estudiantat.upc.edu>
2024-10-23 20:48:07 +0200

    Con condicional de que le ha dado al interruptor

--- Assets/Scripts/PortalOpen.cs
+++ Assets/Scripts/PortalOpen.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class PortalOpen : MonoBehaviour
 {
     public Animator portalAnimation;
+    public GameObject player;
+    private PortalKey portalKey;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        portalKey = player.GetComponent<PortalKey>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && portalKey.HasKey())
         {
             portalAnimation.SetTrigger("open");
         }
--- Assets/Scripts/PortalUse.cs
+++ Assets/Scripts/PortalUse.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class PortalUse : MonoBehaviour
 {
     public Transform secondTP;
-    public Transform player;
+    public GameObject player;
+    private Transform playerTransform;
+    private PortalKey portalKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = player.GetComponent<Transform>();
+        portalKey = player.GetComponent<PortalKey>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && portalKey.HasKey())
         {
-            player.position = new Vector3(secondTP.position.x, secondTP.position.y, secondTP.position.z);
-            player.rotation = Quaternion.Euler(player.rotation.x, secondTP.rotation.y, player.rotation.z);
+            playerTransform.position = new Vector3(secondTP.position.x, secondTP.position.y, secondTP.position.z);
+            playerTransform.rotation = Quaternion.Euler(playerTransform.rotation.x, secondTP.rotation.y, playerTransform.rotation.z);
         }
     }
 }
