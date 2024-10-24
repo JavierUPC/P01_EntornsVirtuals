aa92334615847eb401e4b9b08cfa27083c12d0ec
gmlluc <lluc.grifol@estudiantat.upc.edu>
2024-10-24 10:56:24 +0200

    Codigo zoomcam final 1

--- Assets/Scripts/ZoomCam.cs
+++ Assets/Scripts/ZoomCam.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class ZoomCam : MonoBehaviour
 {
-    
-    public 
+    public Camera playerCamera; 
+    public float zoomedFOV = 30f; //fov del zoom
+    public float normalFOV = 60f; //fov normal
+    public float zoomSpeed = 2f; //velocidad zoom
 
+    private bool isZoomed = false;
 
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        ZoomCam();
+        
+        if (Input.GetKeyDown(KeyCode.Mouse2)) 
+        {
+            isZoomed = !isZoomed; //activar i desactivar
+        }
+
+        //Fov camara
+        if (isZoomed)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, zoomedFOV, Time.deltaTime * zoomSpeed);
+        }
+        else
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, normalFOV, Time.deltaTime * zoomSpeed);
+        }
     }
-
-    private void ZoomCam()
-    {
-
-    }
-
 }
